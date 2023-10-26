122aed091ffe02ea5860df9851d2bc9d494cac07
Dmytro Petyshyn <dev6012@gmail.com>
2023-10-27 02:20:50 +0300

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import schedule from 'node-schedule';
+import fetch from 'node-fetch';
+
+const job = schedule.scheduleJob('*/28 * * * *', async () => {
+  try {
+    const response = await fetch(process.env.REQ_URL);
+    if (response.ok) {
+      console.log('✅ GET request to API successful');
+    } else {
+      console.error(
+        '❌ GET request to API failed:',
+        response.status,
+        response.statusText
+      );
+    }
+  } catch (error) {
+    console.error('❌ Error making GET request:', error);
+  }
+});
